eaf54bcc018f3a6caa7ef1cf313844c2277db63b
Anupriya Goyal <anupriyagoyal14@gmail.com>
2020-01-22 14:51:00 -0500

    Update TwoSumLessThan_k.java

--- TwoSumLessThan_k.java
+++ TwoSumLessThan_k.java
@@ -1,3 +1,5 @@
+/* using two pointer approach */
+
 class Solution {
     public int twoSumLessThanK(int[] A, int K) {
         Arrays.sort(A);
@@ -6,10 +8,10 @@ class Solution {
         int ans = -1;
         while (i < j) {
             if (A[i] + A[j] >= K) {
-                j --;
+                j --;  //j represent pointer moving from right side of array
             } else {
                 ans = Math.max(ans, A[i] + A[j]);
-                i ++;
+                i ++; //i represent pointer moving from left side of array
             }
         }
         return ans;
