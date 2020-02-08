e15ef13e3d8528b8619e9352c0c00c9d88c11dd1
Anupriya Goyal <anupriyagoyal14@gmail.com>
2020-02-08 18:48:46 -0500

    Add files via upload
    Lowest Common Ancestor in Binary Tree problem

---
+++ LowestCommonAncestor.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// Using Recursion
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(root==null)
+        {
+            return null;
+        }
+        
+        if(root==p || root == q)
+        {
+            return root;
+        }
+        
+        TreeNode left= lowestCommonAncestor(root.left,p,q);
+        TreeNode right= lowestCommonAncestor(root.right, p, q);
+        if(left!=null && right != null)
+        {
+            return root;
+        }
+        if(left==null && right == null)
+        {
+            return null;
+        }
+        
+        return left!= null?left:right;
+    }
+}
