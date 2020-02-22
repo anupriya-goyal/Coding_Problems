c876264ca97501125cc98ce04df2a3e12ba15184
Anupriya Goyal <anupriyagoyal14@gmail.com>
2020-02-21 21:23:32 -0500

    Add files via upload

---
+++ CombinationSum.java
@@ -0,0 +1,41 @@
+// Combination Sum using DFS
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+      List<List<Integer>> results= new ArrayList<>();
+        
+        if(candidates==null || candidates.length==0)
+        {
+            return results;
+        }
+        
+        Arrays.sort(candidates);
+        
+        List<Integer> combination= new ArrayList<>();
+        
+        toFindCombinationsToTarget(results, combination, candidates, target,0);
+        return results;
+        
+}
+ 
+    public void toFindCombinationsToTarget(List<List<Integer>> results, List<Integer> combination, int[] candidates, int target, int startIndex)
+    {
+        if(target==0)
+        {
+            results.add(new ArrayList<>(combination));
+        }
+        
+        for(int i=startIndex;i<candidates.length;i++)
+        {
+            if(candidates[i]>target)
+            {
+                break;
+            }
+            combination.add(candidates[i]);
+            toFindCombinationsToTarget(results, combination, candidates, target-candidates[i],i);
+            
+            combination.remove(combination.size()-1);
+        }
+    }
+    
+}
